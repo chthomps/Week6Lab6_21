ae2e75920db76efb68afe9a036a2c61e2fa02eac
chthomps <67965487+chthomps@users.noreply.github.com>
2021-08-12 20:02:35 -0500

    Add files via upload

---
+++ Exercise4.java
@@ -0,0 +1,14 @@
+
+public class Exercise4 {
+
+	public static void main(String[] args) {
+		
+		int [] array = { 1, 14, 15, 24, 55, 59, 73, 90, 94, 99 } ; 
+
+	}
+
+}
+// middle element: 55, left bound index: 0, right bound index: 9
+// middle element: 90, left bound index: 5, right bound index: 9
+// middle element: 59, left bound index: 5, right bound index: 6
+// middle element: 73, left bound index: 6, right bound index: 6
